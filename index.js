549d82f4720cf19879397dcb3ac383a2d86f3d22
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-07-17 11:23:02 +0200

    Refactors code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { JSDOM } = require("jsdom");
 
-const libraryDir = path.join(
+const LIBRARY_DIR = path.join(
   __dirname,
   "node_modules",
   "@wordpress",
@@ -10,84 +10,92 @@ const libraryDir = path.join(
   "src",
   "library",
 );
-
 const ICONS_PER_ROW = 16;
 const ICON_SIZE = 24;
+const OUTPUT_DIR = path.join(__dirname, "icons");
 
-const files = fs.readdirSync(libraryDir);
-const data = [];
-const outputDir = path.join(__dirname, "icons");
-
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
+if (!fs.existsSync(OUTPUT_DIR)) {
+  fs.mkdirSync(OUTPUT_DIR);
 }
 
-files
-  .filter((file) => file.endsWith(".js"))
-  .forEach((file) => {
-    const filePath = path.join(libraryDir, file);
-    let fileContents = fs.readFileSync(filePath, "utf8");
-    try {
-      fileContents = fileContents.replace(/{ {/g, '"').replace(/} }/g, '"');
-      const dom = new JSDOM(`<body>${fileContents}</body>`);
-      const svgElement = dom.window.document.querySelector("svg");
+function parseSvgContent(filePath) {
+  const fileContents = fs
+    .readFileSync(filePath, "utf8")
+    .replace(/{ {/g, '"')
+    .replace(/} }/g, '"');
+  const dom = new JSDOM(`<body>${fileContents}</body>`);
+  const svgElement = dom.window.document.querySelector("svg");
 
-      if (svgElement) {
-        let innerSVGContent = "";
-        const viewBox = svgElement.getAttribute("viewBox");
-        let minWidth = 0;
-        let minHeight = 0;
-        let width = ICON_SIZE;
-        let height = ICON_SIZE;
+  if (svgElement) {
+    const viewBox = svgElement.getAttribute("viewBox");
 
-        if (viewBox) {
-          [minWidth, minHeight, width, height] = viewBox.split(" ").map(Number);
-        }
+    const [minWidth, minHeight, width, height] = viewBox
+      ? viewBox.split(" ").map(Number)
+      : [0, 0, ICON_SIZE, ICON_SIZE];
 
-        Array.from(svgElement.children).forEach((child) => {
-          const serializer = new dom.window.XMLSerializer();
-          innerSVGContent += serializer.serializeToString(child);
-        });
+    const iconPath = Array.from(svgElement.children)
+      .map((child) => new dom.window.XMLSerializer().serializeToString(child))
+      .join("")
+      .replace(/fillrule/g, "fill-rule")
+      .replace(/cliprule/g, "clip-rule")
+      .replace(/xmlns=\"(.*?)\" /g, "");
 
-        const name = path.basename(file, ".js");
-        const iconPath = innerSVGContent
-          .replace(/fillrule/g, "fill-rule")
-          .replace(/cliprule/g, "clip-rule")
-          .replace(/xmlns=\"(.*?)\" /g, "");
+    return { minWidth, minHeight, width, height, path: iconPath };
+  }
 
-        data.push({ name, path: iconPath, minWidth, minHeight, width, height });
-      }
-    } catch (error) {
-      console.error(`Error processing ${file}: ${error}`);
-    }
+  return null;
+}
+
+function generateIconFiles(data) {
+  data.forEach((icon) => {
+    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="${icon.minWidth} ${icon.minHeight} ${icon.width} ${icon.height}">${icon.path}</svg>`;
+    fs.writeFileSync(path.join(OUTPUT_DIR, `${icon.name}.svg`), svgContent);
   });
 
-data.forEach((icon) => {
-  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="${icon.minWidth} ${icon.minHeight} ${icon.width} ${icon.height}">${icon.path}</svg>`;
-  fs.writeFileSync(path.join(__dirname, "icons", `${icon.name}.svg`), svg);
-});
+  console.log(
+    `- ${data.length} icons have been successfully created and saved to the icons directory.`,
+  );
+}
 
-console.log(
-  "- SVG icons have been successfully created and saved to the icons directory.",
-);
+function generateGrid(data) {
+  const allIconsSvgContent = data
+    .map((icon, index) => {
+      const x = (index % ICONS_PER_ROW) * ICON_SIZE;
+      const y = Math.floor(index / ICONS_PER_ROW) * ICON_SIZE;
+      return `<svg id="${icon.name}" x="${x}" y="${y}" width="${ICON_SIZE}" height="${ICON_SIZE}" viewBox="${icon.minWidth} ${icon.minHeight} ${icon.width} ${icon.height}">${icon.path}</svg>`;
+    })
+    .join("");
 
-const allIconsSvgContent = data
-  .map((icon, index) => {
-    const x = (index % ICONS_PER_ROW) * ICON_SIZE;
-    const y = Math.floor(index / ICONS_PER_ROW) * ICON_SIZE;
-    return `<svg id="${icon.name}" x="${x}" y="${y}" width="${ICON_SIZE}" height="${ICON_SIZE}" viewBox="${icon.minWidth} ${icon.minHeight} ${icon.width} ${icon.height}">${icon.path}</svg>`;
-  })
-  .join("");
+  const gridWidth = ICONS_PER_ROW * ICON_SIZE;
+  const gridHeight = Math.ceil(data.length / ICONS_PER_ROW) * ICON_SIZE;
+  const allIconsSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="${gridWidth}" height="${gridHeight}" viewBox="0 0 ${gridWidth} ${gridHeight}">${allIconsSvgContent}</svg>`;
 
-// Calculate the grid width and height
-const totalIcons = data.length;
-const rows = Math.ceil(totalIcons / ICONS_PER_ROW);
-const gridWidth = ICONS_PER_ROW * ICON_SIZE;
-const gridHeight = rows * ICON_SIZE;
+  fs.writeFileSync(path.join(OUTPUT_DIR, "all.svg"), allIconsSvg);
+  console.log(
+    "- SVG icons have been successfully created and compiled into a grid.",
+  );
+}
 
-const allIconsSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="${gridWidth}" height="${gridHeight}" viewBox="0 0 ${gridWidth} ${gridHeight}">${allIconsSvgContent}</svg>`;
-fs.writeFileSync(path.join(outputDir, "all.svg"), allIconsSvg);
+function start() {
+  const files = fs
+    .readdirSync(LIBRARY_DIR)
+    .filter((file) => file.endsWith(".js"));
 
-console.log(
-  "- SVG icons have been successfully created and compiled into a grid.",
-);
+  const data = files
+    .map((file) => {
+      const filePath = path.join(LIBRARY_DIR, file);
+      const svgData = parseSvgContent(filePath);
+      if (svgData) {
+        return { name: path.basename(file, ".js"), ...svgData };
+      }
+      return null;
+    })
+    .filter(Boolean);
+
+  generateIconFiles(data);
+  generateGrid(data);
+
+  return data;
+}
+
+start();
